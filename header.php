e20e528c963b1a88e721c6dc49977652f9a6b33a
Md Rasel Uddin <35773374+Md-Rasel-Uddin@users.noreply.github.com>
2018-04-04 23:17:16 +0600

    header.php
    john's market/slice/header.php

---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<title>John's Market</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<script>
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+  </script>
+  <script>
+  $( function() {
+    $( "#dialog" ).dialog();
+  } );
+  </script>
+</head>
+<body>
+	<div class="container">  
+		<div class="row">   <!-- header_top -->
+			<div class="col-md-6">
+				<div class="header_left">
+					<img src="images/logo.png">
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="pull-right header_right">
+					<ul>
+						<li><a href=""><i class="fa fa-facebook"></i></a></li>
+						<li><a href=""><i class="fa fa-linkedin"></i></a></li>
+						<li><a href=""><i class="fa fa-twitter"></i></a></li>
+						<li><a href=""><i class="fa fa-youtube"></i></a></li>
+					</ul>
+				</div>
+			</div>			
+		</div>
+		<div class="header_bottom">
+			<div class="row">   <!-- header_bottom -->
+				<div class="col-md-8">
+					<div class="header_left">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="about.php">About</a></li>
+							<li><a href="experience.php">Experience</a></li>
+							<li><a href="contact.php">Contact</a></li>
+							<li><a href="blog.php">Blog</a></li>
+							<li><a href="#">E-commerce</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="header_right">
+						<table>
+							<tr>
+								<td><input type="text" name="search"></td>
+								<td><button>Search</button></td>
+							</tr>
+						</table>
+					</div>
+				</div>			
+			</div>	
+		</div>	
+	</div>
