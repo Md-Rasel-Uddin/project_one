1c3995143d0a57c06614d26c34ff702ec22e79d5
Md Rasel Uddin <35773374+Md-Rasel-Uddin@users.noreply.github.com>
2018-04-04 23:11:57 +0600

    footer.php
    john's/slice/footer.php

---
+++ footer.php
@@ -0,0 +1,84 @@
+<div class="divider"></div>
+<div class="container"> 
+		<div class="row footer_top">   <!-- footer_top -->
+			<div class="col-md-3">
+				<div class="box_1">
+					<ul>
+						<li>List of Appliances</li>
+						<li><a href="">Fridge</a></li>
+						<li><a href="">Telivision</a></li>
+						<li><a href="">Air Condition</a></li>
+						<li><a href="">Washing Machine</a></li>
+						<li><a href="">Oven</a></li>
+						<li><a href="">Water Hitter</a></li>
+						<li><a href="">Generator</a></li>						
+						<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="">More</a></li>
+					</ul>					
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="box_1">
+					<ul>
+						<li>List of Furniture</li>
+						<li><a href="">Fridge</a></li>
+						<li><a href="">Telivision</a></li>
+						<li><a href="">Air Condition</a></li>
+						<li><a href="">Washing Machine</a></li>
+						<li><a href="">Oven</a></li>
+						<li><a href="">Water Hitter</a></li>
+						<li><a href="">Generator</a></li>
+						<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="">More</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="box_1">
+					<ul>
+						<li>List of Stationary</li>
+						<li><a href="">Fridge</a></li>
+						<li><a href="">Telivision</a></li>
+						<li><a href="">Air Condition</a></li>
+						<li><a href="">Washing Machine</a></li>
+						<li><a href="">Oven</a></li>
+						<li><a href="">Water Hitter</a></li>
+						<li><a href="">Generator</a></li>
+						<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="">More</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="box_1">
+					<ul>
+						<li>List of Goods</li>
+						<li><a href="">Fridge</a></li>
+						<li><a href="">Telivision</a></li>
+						<li><a href="">Air Condition</a></li>
+						<li><a href="">Washing Machine</a></li>
+						<li><a href="">Oven</a></li>
+						<li><a href="">Water Hitter</a></li>
+						<li><a href="">Generator</a></li>
+						<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="">More</a></li>
+					</ul>
+				</div>
+			</div>				
+		</div>	
+		<div class="row footer_bottom">   <!-- footer_bottom -->
+			<div class="col-md-5">
+				<div class="footer_left">
+					<p class="foot_paddiing">John'sMarket website in Uk</p>
+				</div>
+			</div>
+			<div class="col-md-2">
+				<div class="footer_left">
+					<img src="images/logo.png">
+				</div>
+			</div>
+			<div class="col-md-5">
+				<div class="footer_right">
+					<p class="foot_paddiing pull-right">copyright &copy; by <span>John'sMarket</span></p>
+				</div>
+			</div>			
+		</div>	
+	</div>
+</body>
+</html>
